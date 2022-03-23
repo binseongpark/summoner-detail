3e4b6a26cbf44fc1d65da909dfaa5a4112c9034d
박성빈 <>
2022-03-24 01:30:18 +0900

    fix .

--- src/Summary.js
+++ src/Summary.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export default function Summary() {
   return (
@@ -7,7 +7,38 @@ export default function Summary() {
       <Body>
         <div>1</div>
         <MostChampionContainer>
-          <MostChampion></MostChampion>
+          <MostChampion>
+            <div>
+              <img src="https://opgg-static.akamaized.net/images/lol/champion/Vex.png?image=q_auto,f_webp,w_auto&v=1648022284663" />
+            </div>
+            <div>
+              <div>벡스</div>
+              <div>
+                <WinRateSpan>70%</WinRateSpan>
+                <span> (7승 3패)</span>
+                <span>13.01 평점</span>
+              </div>
+            </div>
+          </MostChampion>
+          <MostChampion>
+            <div>
+              <img src="https://opgg-static.akamaized.net/images/lol/champion/Vex.png?image=q_auto,f_webp,w_auto&v=1648022284663" />
+            </div>
+            <div>
+              <div>벡스</div>
+              <div>
+                <WinRateSpan>70%</WinRateSpan>
+                <span> (7승 3패)</span>
+                <span>13.01 평점</span>
+              </div>
+            </div>
+          </MostChampion>
+          <MostChampion isEmpty>
+            <div>
+              <img src="images/empty-champion.svg" />
+            </div>
+            <div>챔피언 정보가 없습니다.</div>
+          </MostChampion>
         </MostChampionContainer>
         <div>
           <div className="title">선호 포지션 (랭크)</div>
@@ -104,8 +135,91 @@ const Body = styled.div`
 const MostChampionContainer = styled.div`
   padding-left: 16px;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
-const MostChampion = styled.div``;
+const MostChampion = styled.div`
+  display: flex;
+  &:not(:nth-of-type(1)) {
+    margin-top: 12px;
+  }
+  & > div {
+    &:nth-of-type(1) {
+      width: 34px;
+      height: 34px;
+      border-radius: 45px;
+      overflow: hidden;
+
+      & > img {
+        width: 100%;
+      }
+    }
+    &:nth-of-type(2) {
+      padding-left: 8px;
+      ${(props) => {
+        if (props.isEmpty) {
+          return css`
+            font-family: NanumBarunGothicOTF;
+            font-size: 11px;
+            font-weight: normal;
+            font-stretch: normal;
+            font-style: normal;
+            line-height: normal;
+            letter-spacing: normal;
+            color: var(--warm-grey-two);
+            display: flex;
+            align-items: center;
+          `;
+        } else {
+          return css`
+            & > div {
+              &:nth-of-type(1) {
+                font-family: NanumBarunGothicOTF;
+                font-size: 14px;
+                font-weight: normal;
+                font-stretch: normal;
+                font-style: normal;
+                line-height: normal;
+                letter-spacing: normal;
+                color: var(--black);
+              }
+              &:nth-of-type(2) {
+                font-family: Helvetica;
+                font-size: 11px;
+                font-weight: normal;
+                font-stretch: normal;
+                font-style: normal;
+                line-height: normal;
+                letter-spacing: normal;
+                color: var(--greyish-brown);
+
+                & > span {
+                  &:nth-of-type(1) {
+                  }
+                  &:nth-of-type(2) {
+                  }
+                  &:nth-of-type(3) {
+                    color: var(--yellow-ochre);
+
+                    &::before {
+                      display: inline-block;
+                      margin-left: 6px;
+                      margin-right: 6px;
+                      content: "";
+                      border-left: 1px solid rgb(205, 210, 210);
+                      height: 12px;
+                      vertical-align: middle;
+                    }
+                  }
+                }
+              }
+            }
+          `;
+        }
+      }}
+    }
+  }
+`;
 
 const LineWinRate = styled.div`
   display: flex;
@@ -177,4 +291,8 @@ const LineWinRate = styled.div`
       }
     }
   }
+`;
+
+const WinRateSpan = styled.span`
+  color: var(--reddish);
 `;
